dcd892ec27f9c8a2c7ea1648c05b6fe792e5771e
Jennova <jennova.technologies@gmail.com>
2020-12-17 18:53:39 +0100

    device.mk: Add PixelLiveWallpaper

--- device.mk
+++ device.mk
@@ -92,6 +92,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
 
+# PixelLiveWallpaper
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
